23f143702b0b7375d214e0d3eefa26efb06d786f
Leonardo Anjos <las4@cin.ufpe.br>
2020-11-18 13:53:07 -0300

    Preparação em turmas para funcionalidade de comparação de desempenho

--- ta-gui/src/app/turmas/turmas.component.ts
+++ ta-gui/src/app/turmas/turmas.component.ts
@@ -8,10 +8,46 @@ import { Turma } from '../../../../common/turma';
 })
 export class TurmasComponent implements OnInit {
   turmas: Turma[] = [];
+  turmasEscolhidas: Turma[] = [];
+  modalOpcoesComparacaoAtivo: boolean = false;
+  modalTurmasAtivo: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
 
+  }
+
+  alternarVisualizacaoOpcoesComparacao(): void {
+    this.modalOpcoesComparacaoAtivo = !this.modalOpcoesComparacaoAtivo;
+    this.turmasEscolhidas = [];
+  }
+
+  alternarVisualizacaoTurmas(): void {
+    this.alternarVisualizacaoOpcoesComparacao();
+    this.modalTurmasAtivo = !this.modalTurmasAtivo;
+  }
+
+  atualizarTurmasEscolhidas(descricaoTurma: string): void {
+    if (!this.turmasEscolhidas.find(turma => turma.descricao === descricaoTurma)) {
+      this.turmasEscolhidas.push(this.turmas.find(turma => turma.descricao === descricaoTurma));
+    } else {
+      this.turmasEscolhidas = this.turmasEscolhidas.filter(turma => turma.descricao !== descricaoTurma);
+    }
+  }
+
+  compararTodas(): void {
+    this.turmasEscolhidas = this.turmas.map(turma => turma);
+  }
+
+  compararUltimasQuatro(): void {
+    this.turmasEscolhidas = [];
+    for (let i = this.turmas.length; this.turmasEscolhidas.length < 4; i--) {
+      this.turmasEscolhidas.push(this.turmas[i]);
+    }
+  }
+
+  compararTurmasEscolhidas(): void {
+    
   }
 }
