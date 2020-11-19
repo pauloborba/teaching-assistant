bcaeb7cfdec642374f0e41609b9f7fe103e358ed
Leonardo Anjos <las4@cin.ufpe.br>
2020-11-18 23:51:15 -0300

    Adição de lógica para criar os gráficos no componente ComparacaoDeDesempenho

--- ta-gui/src/app/comparacao-de-desempenho/comparacao-de-desempenho.component.ts
+++ ta-gui/src/app/comparacao-de-desempenho/comparacao-de-desempenho.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, QueryList, ViewChildren } from '@angular/core';
+
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-comparacao-de-desempenho',
   templateUrl: './comparacao-de-desempenho.component.html',
   styleUrls: ['./comparacao-de-desempenho.component.css']
 })
-export class ComparacaoDeDesempenhoComponent implements OnInit {
-
+export class ComparacaoDeDesempenhoComponent implements AfterViewInit {
+  @ViewChildren('grafico') graficos: QueryList<any>;
+  turmas: any[] = [];
+  
   constructor() { }
-
-  ngOnInit(): void {
+  
+  ngAfterViewInit(): void {
+    const canvasMedia = this.graficos.find(c => c.nativeElement.id === 'media').nativeElement;
+    this.criarGrafico(canvasMedia, 'Média', this.turmas.map(turma => turma.media));
+    
+    const canvasReprovacao = this.graficos.find(c => c.nativeElement.id === 'reprovacao').nativeElement;
+    this.criarGrafico(canvasReprovacao, 'Reprovação', this.turmas.map(turma => turma.reprovacao));
   }
 
+  criarGrafico(canvas: any, nome: string, dados: number[]): Chart {
+    return new Chart(canvas, {
+      type: 'line',
+      data: {
+        labels: this.turmas.map(turma => turma.descricao),
+        datasets: [
+          {
+            label: nome,
+            data: dados
+          }
+        ]
+      }
+    })
+  }
 }
