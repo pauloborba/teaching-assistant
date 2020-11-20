25098ac127a0b7c8f11cc6165d632fefb0da7093
eulaliaaires <memgra@cin.ufpe.br>
2020-11-19 23:11:13 -0300

    ajuste de sintaxe na classe de matrícula

--- common/matricula.ts
+++ common/matricula.ts
@@ -3,25 +3,25 @@ import { Avaliacao } from "../ta-server/avaliacao"
 import { RespostaDeRoteiro } from "../ta-server/respostaderoteiro"
 
 export class Matricula{
-    aluno: Aluno
-    avaliacoes: Avaliacao[] = []
-    autoAvaliacoes: Avaliacao[] = []
-    monitor: Aluno
-    respostasDeRoteiros: RespostaDeRoteiro[] = []
+    aluno: Aluno;
+    avaliacoes: Avaliacao[] = [];
+    autoAvaliacoes: Avaliacao[] = [];
+    monitor: Aluno;
+    respostasDeRoteiros: RespostaDeRoteiro[] = [];
     
     getAluno(): Aluno{
         return this.aluno
-    }
+    };
     getAvaliacoes(): Avaliacao[]{
         return this.avaliacoes
-    }
+    };
     getAutoAvaliacoes(): Avaliacao[]{
         return this.autoAvaliacoes
-    }
+    };
     getMonitor(): Aluno {
         return this.monitor      
-    }
+    };
     getRespostasDeRoteiros(): RespostaDeRoteiro[]{
         return this.respostasDeRoteiros
-    }
+    };
 }
