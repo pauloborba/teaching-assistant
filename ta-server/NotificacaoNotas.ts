2b6a8a11b097838ecbbff878a6908e53e789fa5b
felipe gomes <felipinhogoma@gmail.com>
2020-11-23 02:25:24 -0300

    Criação da função que ressalta as diferenças nas metas e stub do texto que diz o conceito de cada meta do aluno

--- ta-server/NotificacaoNotas.ts
+++ ta-server/NotificacaoNotas.ts
@@ -1,34 +1,51 @@
-import  { Turma } from '../common/turma'
+import { Turma } from '../common/turma'
 import { EmailSender } from './EmailSender'
+import { Matricula } from '../common/matricula'
 
 export class NotificacaoNotas {
   emailSender: EmailSender = new EmailSender();
 
   enviarNotificação(turma: Turma): boolean {
-    var porcentagensDeConceitoDasMetas: Map<String,Map<string, number>> = this.gerarPercentsDeMetas(turma);
+    var porcentagensDeConceitoDasMetas: Map<String, Map<string, number>> = this.gerarPercentsDeMetas(turma);
     var mediaTurma: number = turma.getMedia();
-    for(let matricula of turma.matriculas){
-      var texto: String = "" ;
-      for(let a of matricula.avaliacoes){
-        
-      }
-    
-    
-    return this.emailSender.enviarEmail(matricula.aluno.email,"", "");
+    for (let matricula of turma.matriculas) {
+      var texto: string = this.conceitoDasMetas(matricula) + this.ressaltarDiferencas(matricula, porcentagensDeConceitoDasMetas);
+      
+
+
+      return this.emailSender.enviarEmail(matricula.aluno.email, "", texto);
     }
   }
 
-  gerarPercentsDeMetas(turma: Turma): Map<String,Map<string,number>>{
-    var metasPercents: Map<String,Map<string,number>>;
-    for(let meta of turma.metas){
-      var percents: Map<string,number>;
-      percents.set("MA", turma.getPercentual(meta,"MA"));
-      percents.set("MPA", turma.getPercentual(meta,"MPA"));
-      percents.set("MANA", turma.getPercentual(meta,"MANA"));
-      metasPercents.set(meta,percents);
+  ressaltarDiferencas(matricula: Matricula, porcentagensDeConceitoDasMetas: Map<String, Map<string, number>>): string {
+    var texto:string = "" ;
+    for (let a of matricula.avaliacoes) {
+      texto += " Para a meta: '" + a.meta + "' seu conceito foi: " + a.nota + "\n"
+        + "Turma: " 
+        + "MA - " + porcentagensDeConceitoDasMetas.get(a.meta).get("MA") + ", "
+        + "MPA - " + porcentagensDeConceitoDasMetas.get(a.meta).get("MPA") + ", "
+        +  "MANA : " + porcentagensDeConceitoDasMetas.get(a.meta).get("MA") + "\n \n"
+    }
+    return ""
+  }
+
+  gerarPercentsDeMetas(turma: Turma): Map<String, Map<string, number>> {
+    var metasPercents: Map<String, Map<string, number>>;
+    for (let meta of turma.metas) {
+      var percents: Map<string, number>;
+      percents.set("MA", turma.getPercentual(meta, "MA"));
+      percents.set("MPA", turma.getPercentual(meta, "MPA"));
+      percents.set("MANA", turma.getPercentual(meta, "MANA"));
+      metasPercents.set(meta, percents);
     }
     return metasPercents;
   }
-   
+
+  //Texto que diz qual conceito o aluno tirou em cada meta {{stub}}
+  conceitoDasMetas(matricula: Matricula): string {
+    
+    return "";
   }
-  
+
+}
+
