b9db0cbf5d0d694365a703cd323bd859f23dfbdc
rodrigo <rfop@cin.ufpe.br>
2020-11-24 14:14:29 -0300

    criei a gui de turmas

---
+++ ta-gui/src/app/turmas/editar-turma/editar-turma.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-turma',
+  templateUrl: './editar-turma.component.html',
+  styleUrls: ['./editar-turma.component.css']
+})
+export class EditarTurmaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  editarTurma(){
+    console.log("editar");
+  }
+}
