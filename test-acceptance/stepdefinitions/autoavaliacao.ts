71aa5cbe4f2dc255f2488b67247f0e8b3bf4e3e2
vaoliveira <vao@cin.ufpe.br>
2020-11-24 21:23:52 -0300

    inicio da implementação dos testes

--- test-acceptance/stepdefinitions/autoavaliacao.ts
+++ test-acceptance/stepdefinitions/autoavaliacao.ts
@@ -4,5 +4,24 @@ let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
 defineSupportCode(function ({ Given, When, Then }) {
+    Given(/^I am at the auto-avaliacao page$/, async () => {
+        await browser.get("http://localhost:4200/");
+        await expect(browser.getTitle()).to.eventually.equal('TaGui');
+        await $("a[name='auto-avaliacao']").click();
+    });
 
-}
+    Given(/^I am at the "([^\"]*)" class$/, async (turma) => {
+        await $("button[name='notificar']").click();
+        await $("input[name='descricaoTurma']").sendKeys(<string> turma);
+        await $("button[name='selecionar']").click();
+    });
+
+    When(/^I select the topics "([^\"]*)" and "([^\"]*)"$/, async (meta1, meta2) => {
+        await $("input[value='Requisitos']").click();
+        await $("input[value='Refatoração']").click();
+    });
+
+    When(/^I send the self-grade request to all students$/, async () => {
+        await $("button[name='notificarEmail']").click();
+    });
+})
