8a1dc2e2447f5b2ac05a145c3b2f3c308625fffe
ToniMaciel <jaam@cin.ufpe.br>
2020-11-26 02:19:31 -0300

    Adicao de componentes do servidor para adicao de roteiro

--- ta-server/cadastroderoteiros.ts
+++ ta-server/cadastroderoteiros.ts
@@ -1,11 +1,15 @@
-import  {Roteiro } from './roteiro'
+import  {Roteiro } from '../common/roteiro'
 
 export class CadastroDeRoteiros{
        roteiros: Roteiro[] = [];
-       
-       cadastrarRoteiro (roteiro: Roteiro): void{};
+
+       cadastrarRoteiro (roteiro: Roteiro): Roteiro{
+          var result = new Roteiro();
+          result.copyFrom(roteiro);
+          this.roteiros.push(result);
+          return result;
+       }
 
        removerRoteiro (descricao: String ): void{};
-       
-}
 
+}
--- ta-server/ta-server.ts
+++ ta-server/ta-server.ts
@@ -1,14 +1,17 @@
 import express = require('express');
 import bodyParser = require("body-parser");
 
-import {Aluno} from '../common/aluno';
-import {CadastroDeAlunos} from './cadastrodealunos'; 
+/*import {Aluno} from '../common/aluno';
+import {CadastroDeAlunos} from './cadastrodealunos';
 import {Turmas} from './turmas'
-import {Matricula} from '../common/matricula'
+import {Matricula} from '../common/matricula'*/
+import {Roteiro} from '../common/roteiro'
+import {CadastroDeRoteiros} from './cadastroderoteiros';
 
 var taserver = express();
 
 var cadastro: CadastroDeAlunos = new CadastroDeAlunos();
+var cadastroRoteiro: CadastroDeRoteiros = new CadastroDeRoteiros();
 
 var allowCrossDomain = function(req: any, res: any, next: any) {
     res.header('Access-Control-Allow-Origin', "*");
@@ -20,7 +23,7 @@ taserver.use(allowCrossDomain);
 
 taserver.use(bodyParser.json());
 
-taserver.get('/alunos', function (req: express.Request, res: express.Response) {
+/*taserver.get('/alunos', function (req: express.Request, res: express.Response) {
 
 })
 
@@ -32,14 +35,28 @@ taserver.get('/turmas', function (req: express.Request, res: express.Response){
 //recebe um identificador de turma e de aluno e retorna uma matricula
 taserver.get('/matriculas', function (req: express.Request, res: express.Response){
 
+})*/
+
+taserver.get('/roteiros', function (req: express.Request, res: express.Response){
+  res.send(JSON.stringify(cadastroRoteiro.getRoteiros()));
+})
+
+taserver.post('/roteiro', function (req: express.Request, res: express.Response) {
+  var roteiro: Roteiro = <Roteiro> req.body;
+  roteiro = cadastroRoteiro.cadastrarRoteiro(roteiro);
+  if (roteiro) {
+    res.send({"success": "O roteiro foi cadastrado com sucesso"});
+  } else {
+    res.send({"failure": "O roteiro não pode ser cadastrado"});
+  }
 })
 
 var server = taserver.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
-  
+
 function closeServer(): void {
     server.close();
 }
-  
+
 export { server, closeServer }
