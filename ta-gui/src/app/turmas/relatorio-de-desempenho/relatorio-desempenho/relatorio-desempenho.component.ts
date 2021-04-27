3f3a9d7746814d1a7e21d18359a989b76febe16e
Guilherme Afonso <guilherme.asp@live.com>
2021-04-27 05:50:18 -0300

    Adiciona componente relatorio de desempenho

---
+++ ta-gui/src/app/turmas/relatorio-de-desempenho/relatorio-desempenho/relatorio-desempenho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorio-desempenho',
+  templateUrl: './relatorio-desempenho.component.html',
+  styleUrls: ['./relatorio-desempenho.component.css']
+})
+export class RelatorioDesempenhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
