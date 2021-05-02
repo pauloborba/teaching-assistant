e40f42306a88b73c8232840c04f2a467808af7f4
Belchior <bmf@cin.ufpe.br>
2021-05-01 23:12:24 -0300

    Removendo Console Log

--- ta-server/repos/verificaPrazo.ts
+++ ta-server/repos/verificaPrazo.ts
@@ -4,25 +4,17 @@ import { DateLimit } from "../../common/agendamentoDeRoteiro";
 export class VerificaPrazo {
 
     compareDates(dateLimit:DateLimit):boolean{
-        console.log(dateLimit.datafim);
-        
+      
         const format = "DD-MM-YYYY HH:mm:ss";
         var datafimdate = moment(dateLimit.datafim,format);
-        console.log(datafimdate);
         var dataatualdate = new Date();
-        console.log(dataatualdate);
         var datetimeatual = moment(dataatualdate).format(format);
         var datetimefim = moment(datafimdate).format(format);
-       console.log(datetimeatual);
-       console.log(datetimefim);
-       
-       
+      
         if( datetimeatual > datetimefim){
-          console.log("fora do prazo");
           return false;
         }
         else{
-          console.log("dentro do prazo");
           return true;
         }
       }
