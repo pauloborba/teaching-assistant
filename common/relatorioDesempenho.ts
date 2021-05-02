2671594fd66e05f2953e02ade93ec461cec17b4e
Guilherme Afonso <guilherme.asp@live.com>
2021-05-02 04:42:13 -0300

    Adiciona classe para Relatorio de Desempenho

---
+++ common/relatorioDesempenho.ts
@@ -0,0 +1,17 @@
+export class RelatorioDeDesempenho {
+    turma: string;
+    aprovados: number;
+    reprovados: number;
+    aprovadosPorMedia: number;
+    reprovadosPorFalta: number;
+    reprovadosPorNota: number;
+    aguardandoFinal: number;
+    total: number;
+    aprovadosPorcentagem: number;
+    reprovadosPorcentagem: number;
+    aprovadosPorMediaPorcentagem: number;
+    reprovadosPorFaltaPorcentagem: number;
+    reprovadosPorNotaPorcentagem: number;
+    aguardandoFinalPorcentagem: number;
+    totalPorcentagem: number;
+  }
