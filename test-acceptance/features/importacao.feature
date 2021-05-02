a2f96b1cfc240dfb3b10dfd481be4578f2b9207d
Tiago <tscs2@cin.ufpe.br>
2021-05-02 03:50:14 -0300

    Testes de GUI Ok

--- test-acceptance/features/importacao.feature
+++ test-acceptance/features/importacao.feature
@@ -1,34 +1,35 @@
-Feature:As a professor
+Feature:Importação de alunos
+        As a professor
         I want to importar alunos através de uma planilha csv
         So that eu importe vários alunos de uma vez
 
 Scenario: Enviar uma planilha com dados de alunos já cadastrados
 Given eu estou na página de alunos
-Given os alunos "Lucas" e "Alyson" de emails "lucas@cin.ufpe.br" e "alyson@cin.ufpe.br" estão presentes na lista de alunos cadastrados
+Given apenas os alunos "Lucas" e "Alyson" de emails "lucas@cin.ufpe.br" e "alyson@cin.ufpe.br" estão presentes na lista de alunos cadastrados
 When eu vou para a página de importar alunos
-And eu seleciono a planilha "alunos_repetidos.csv"
+And eu seleciono a planilha de nome "alunos_repetidos.csv"
 Then aparece um aviso avisando que "2" dos alunos já estavam cadastrados
-And eu vejo "lucas@cin.ufpe.br", "alyson@cin.ufpe.br", "gil@cin.ufpe.br", "maria@cin.ufpe.br", "claudia@cin.ufpe.br" e "joão@cin.ufpe.br" nos emails da lista de alunos 
+And eu vejo "lucas@cin.ufpe.br", "alyson@cin.ufpe.br", "gil@cin.ufpe.br", "maria@cin.ufpe.br", "claudia@cin.ufpe.br" e "joão@cin.ufpe.br" nos emails da lista de alunos
 
 Scenario: Enviar uma planilha vazia
-Given eu estou na página de importar alunos
+Given eu estou na página de alunos
 Given não existem alunos cadastrados na lista de alunos
-When eu escolho a opção de importar Alunos
-And seleciono uma planilha vazia de nome "planilha-vazia.csv"
-Then aparece uma mensagem de erro avisando que o arquivo selecionado é inválido
-And não existem alunos cadastrados na lista de alunos
+When eu vou para a página de importar alunos
+And eu seleciono uma planilha vazia de nome "planilha-vazia.csv"
+Then aparece uma mensagem de erro escrito "Foi selecionada uma planilha vazia."
+And ainda não existem alunos cadastrados na lista de alunos
 
 Scenario: Enviar uma planilha de alunos com sucesso
-Given eu estou na página importar alunos
+Given eu estou na página de alunos
 Given não existem alunos cadastrados na lista de alunos
-When eu escolho a opção de importar alunos
-And eu seleciono uma planilha com os alunos de email "joão@cin.ufpe.br" e "maria@cin.ufpe.br"
-Then os alunos de email "joão@cin.ufpe.br" e "maria@cin.ufpe.br" aparecem na lista de alunos
+When eu vou para a página de importar alunos
+And eu seleciono a planilha de nome "joao-maria.csv"
+Then os alunos de email "joao@cin.ufpe.br" e "maria@cin.ufpe.br" aparecem na lista de alunos
 
 Scenario: Enviar uma planilha em um formato diferente do esperado
-Given eu estou na página de importar alunos
-Given a aluna de email "julia@cin.ufpe.br" está cadastrada
-When eu escolho a opção de importar alunos
-And eu seleciono o arquivo de uma planilha "sem-email.csv" que não tem os dados do email na coluna de logins
-Then aparece uma mensagem de erro avisando que o arquivo selecionado é inválido
-And a aluna de email "julia@cin.ufpe.br" está cadastrada
+Given eu estou na página de alunos
+Given apenas a aluna de email "julia@cin.ufpe.br" está cadastrada
+When eu vou para a página de importar alunos
+And eu seleciono o arquivo de uma planilha de nome "sem-email.csv" que não tem os dados do email na coluna de logins
+Then aparece uma mensagem de erro escrito "planilha não contém os dados de email de um ou mais alunos!"
+And na página de alunos, a aluna de email "julia@cin.ufpe.br" continua sendo a única cadastrada
--- test-acceptance/stepdefinitions/importacao.ts
+++ test-acceptance/stepdefinitions/importacao.ts
@@ -1,23 +1,37 @@
 import { defineSupportCode } from 'cucumber';
-import { browser, $, element, ElementArrayFinder, by, ElementFinder } from 'protractor';
+import { browser, $, element, ElementArrayFinder, by} from 'protractor';
+
+import request = require("request-promise");
+
+
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 var path = require('path');
 
-function sameEmailCheck(elem, email) { return elem.element(by.name('emaillist')).getText().then(text => text === email);}
+async function removerAlunos(){
+    const list = await element.all(by.name("deletar-aluno"));
+    if(!!list.length){
+       for(const p of list){
+           await p.click();
+       }
+    }
+}
 
 async function assertTamanhoEqual(set,n) {
     await set.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(n));
 }
 
-async function assertEmailExists(list, email) {
-    var sameEmail = list.filter(elem => sameEmailCheck(elem, email));
-    await assertTamanhoEqual(sameEmail,1);
+async function checkEmailExists(list: ElementArrayFinder, email: string){
+    const result = list.filter(async (elem) => {
+        return (await elem.getText()) == email;
+    });
+
+    return result.length != 0;
 }
 
-async function assertAllEmailexists(list, emails){
+async function checkAllEmailsExists(list, emails){
     for(const email in emails){
-        if(!assertEmailExists(list, email)){
+        if(!checkEmailExists(list, email)){
             return false;
         }
     }
@@ -31,19 +45,21 @@ async function criarAluno(nome, email) {
 }
 
 defineSupportCode(function ({ Given, When, Then }) {
+    
     Given(/^eu estou na página de alunos$/, async () => {
         await browser.get("http://localhost:4200/alunos");
         await expect(browser.getTitle()).to.eventually.equal('TaGui');
     });
 
-    Given(/^os alunos "(.*)" e "(.*)" de emails "(.*)" e "(.*)" estão presentes na lista de alunos cadastrados$/, async (nome1, nome2, em1, em2) => {
+    Given(/^apenas os alunos "(.*)" e "(.*)" de emails "(.*)" e "(.*)" estão presentes na lista de alunos cadastrados$/, async (nome1, nome2, em1, em2) => {
+        await removerAlunos();
         // Criar Lucas
         await criarAluno(nome1, em1);
         // Criar Alyson
         await criarAluno(nome2, em2);
         var allAlunos : ElementArrayFinder = element.all(by.name('alunolist'));
-        await assertEmailExists(allAlunos, em1);
-        await assertEmailExists(allAlunos, em2);
+        await expect(checkEmailExists(allAlunos, <string> em1));
+        await checkEmailExists(allAlunos, <string> em2);
     });
 
     When(/^eu vou para a página de importar alunos$/, async () => {
@@ -52,21 +68,87 @@ defineSupportCode(function ({ Given, When, Then }) {
     });
 
 
-    When(/^eu seleciono a planilha "(.*)"$/, async (planilha) =>{
+    When(/^eu seleciono a planilha de nome "(.*)"$/, async (planilha) =>{
         var arquivo = `../planilhas/${planilha}`;
         var caminho = path.resolve(__dirname, arquivo);
         await element(by.name('csv')).sendKeys(caminho);
     });
     
     Then(/^aparece um aviso avisando que "(\d*)" dos alunos já estavam cadastrados$/, async (numero) => {
-        await browser.sleep(5000);
+        await browser.sleep(1000);
+        let alert = await browser.switchTo().alert();
+        let texto = await alert.getText();
+        await expect(texto == `Dos alunos da planilha, já haviam ${numero} cadastrados.`);
+        await alert.accept();
     });
     
     Then(/^eu vejo "(.*)", "(.*)", "(.*)", "(.*)", "(.*)" e "(.*)" nos emails da lista de alunos$/, async (em1, em2, em3, em4, em5, em6) => {
         await browser.get("http://localhost:4200/alunos");
         var allAlunos : ElementArrayFinder = element.all(by.name('alunolist'));
         var allEmails : string[] = [<string> em1, <string> em2, <string> em3, <string> em4, <string> em5, <string> em6];
-        await assertAllEmailexists(allAlunos, allEmails);
+        expect(checkAllEmailsExists(allAlunos, allEmails));
+    });
+
+    Given(/^não existem alunos cadastrados na lista de alunos$/, async () => {
+        await removerAlunos();
+        var allAlunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await assertTamanhoEqual(allAlunos,0);
+    });
+
+    When(/^eu seleciono uma planilha vazia de nome "(.*)"$/, async (planilha) =>{
+        var arquivo = `../planilhas/${planilha}`;
+        var caminho = path.resolve(__dirname, arquivo);
+        await element(by.name('csv')).sendKeys(caminho);
+    });
+
+    Then(/^aparece uma mensagem de erro escrito "(.*)"$/, async (msg) => {
+        await browser.sleep(1000);
+        let alert = await browser.switchTo().alert();
+        let texto = await alert.getText();
+        await expect(texto == `${msg}`);
+        await alert.accept();
+    });
+
+    Then(/^ainda não existem alunos cadastrados na lista de alunos$/, async () => {
+        var allAlunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await assertTamanhoEqual(allAlunos,0);
+    });
+
+    Then(/^os alunos de email "(.*)" e "(.*)" aparecem na lista de alunos$/, async (em1, em2) => {
+        await browser.get("http://localhost:4200/alunos");
+        var allAlunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        var allEmails : string[] = [<string> em1, <string> em2];
+        expect(await checkAllEmailsExists(allAlunos, allEmails));
+    });
+
+    Given(/^na página de alunos, apenas a aluna de email "(.*)" está cadastrada$/, async (em) => {
+        await browser.get("http://localhost:4200/alunos");
+        await removerAlunos();
+        await criarAluno('julia', 'julia@cin.ufpe.br');
+        var allAlunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await assertTamanhoEqual(allAlunos, 1);
+        await expect(checkEmailExists(allAlunos, <string> em));
+    });
+    
+    When(/^eu seleciono o arquivo de uma planilha de nome "(.*)" que não tem os dados do email na coluna de logins$/, async (planilha) => {
+        var arquivo = `../planilhas/${planilha}`;
+        var caminho = path.resolve(__dirname, arquivo);
+        await element(by.name('csv')).sendKeys(caminho);
+    });
+
+    Then(/^apenas a aluna de email "(.*)" está cadastrada$/, async (em) => {
+        await browser.get("http://localhost:4200/alunos");
+        await removerAlunos();
+        await criarAluno("julia", <string> em);
+        var allAlunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        expect(await checkEmailExists(allAlunos, <string>em));
+    });
+
+    Given(/^na página de alunos, a aluna de email "(.*)" continua sendo a única cadastrada$/, async (em) => {
+        await browser.get("http://localhost:4200/alunos");
+        var allAlunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await assertTamanhoEqual(allAlunos, 1);
+        expect(await checkEmailExists(allAlunos, <string>em));
     });
 
     
