229f5b14f353cd9efadeef3bdce23e3072f1abf9
pedrohrarruda <43816596+pedrohrarruda@users.noreply.github.com>
2021-05-03 00:06:44 -0300

    limpeza em ta-server

--- ta-server/ta-server.ts
+++ ta-server/ta-server.ts
@@ -17,46 +17,7 @@ import { RespostaDeRoteiro } from './respostaderoteiro';
 import { RespostaDeQuestao } from './respostadequestao';
 import { ImportacaoDeNotas } from './importacaodenotas';
 
- var taserver = express();/*
-// Stub para popular o front-end com alunos de uma turma
-var stub_turma1 = new Turma();
-var stub_turma2 = new Turma();
-stub_turma1.descricao = "ESS";
-stub_turma2.descricao = "Compiladores";
-stub_turma1.metas = ["Requisitos", "Refatoração"];
-stub_turma2.metas = ["Meta1", "Meta2"]
-var stub_matricula1 = new Matricula();
-var stub_matricula2 = new Matricula();
-var stub_aluno1 = new Aluno();
-var stub_aluno2 = new Aluno();
-stub_aluno1.nome = "João";
-stub_aluno1.cpf = "123";
-stub_aluno1.email = "joao@cin.ufpe.br";
-stub_aluno2.nome = "Maria";
-stub_aluno2.cpf = "456";
-stub_aluno2.email = "maria@cin.ufpe.br";
-stub_matricula1.aluno = stub_aluno1;
-stub_matricula2.aluno = stub_aluno2;
-var stub_autoavaliacao1 = new Avaliacao();
-var stub_autoavaliacao2 = new Avaliacao();
-var stub_autoavaliacao3 = new Avaliacao();
-var stub_autoavaliacao4 = new Avaliacao();
-stub_autoavaliacao1.meta = "Requisitos";
-stub_autoavaliacao1.nota = "";
-stub_autoavaliacao2.meta = "Refatoração";
-stub_autoavaliacao2.nota = "";
-stub_autoavaliacao3.meta = "Requisitos";
-stub_autoavaliacao3.nota = "MA";
-stub_autoavaliacao4.meta = "Refatoração";
-stub_autoavaliacao4.nota = "";
-stub_matricula1.autoAvaliacoes = [stub_autoavaliacao1, stub_autoavaliacao2];
-stub_matricula2.autoAvaliacoes = [stub_autoavaliacao3, stub_autoavaliacao4];
-stub_turma1.matriculas = [stub_matricula1, stub_matricula2];
-stub_turma2.matriculas = [stub_matricula2];
-// var sender = new EmailSender();
-*/
-
-
+var taserver = express();
 // stub para turmas
 var stub_turma1 = new Turma("");
 var stub_turma2 = new Turma("");
@@ -290,16 +251,16 @@ taserver.delete('/aluno', function(req: express.Request, res: express.Response){
 taserver.put('/importacaodenota', function (req: express.Request, res: express.Response) {
   let planilha = req.body;
   let avaliacoes = importacao.metaGroup(planilha);
-  console.log (avaliacoes)
   let turma = <string>req.query.turma;
-  //console.log('test3',turma);
   let ok: boolean = false;
-  ok = importacao.importar(turmas.getTurma(turma),avaliacoes)
+
+  ok = importacao.importar(turmas.getTurma(turma), avaliacoes)
   if (ok == true) {
      res.send({"success": "A planilha foi importada com sucesso"});
   } else {
      res.send({"failure": "A planilha não foi importada"});
   }
+  
 });
 
 taserver.get('/importacaodenota', function(req: express.Request, res: express.Response) {
