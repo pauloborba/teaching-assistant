bb99cb903b632af518e318810c7c48b40dbaa8ca
Lucas Asafe <lavn@cin.ufpe.br>
2021-05-04 05:32:19 -0300

    adding stubs

--- ta-server/routes/matriculas.ts
+++ ta-server/routes/matriculas.ts
@@ -1,10 +1,56 @@
 import { Request, Response, Router } from 'express';
+import { Aluno } from '../../common/aluno';
 import { Avaliacao } from '../../common/avaliacao';
 import { Matricula } from '../../common/matricula';
 import { Matriculas } from '../repos/matriculas';
 
 const matriculasRoute = Router();
 const matriculasRepo: Matriculas = new Matriculas();
+
+const alunosStub = [
+  {
+    nome: 'Adriana Vitória Ferreira',
+    cpf: '815.098.557-34',
+    email: 'adrianavitoriaferreira_@uel.br',
+  },
+  {
+    nome: 'Tiago Julio Silva',
+    cpf: '249.897.251-07',
+    email: 'tiagojuliosilva-79@tec3.com.br',
+  }
+];
+
+const avaliacao1: Avaliacao = {
+  meta: "Gerência de Req.",
+  nota: "MA",
+  copyFrom: null
+}
+const avaliacao2: Avaliacao = {
+  meta: "Gerência de Proj.",
+  nota: "MA",
+  copyFrom: null
+}
+const avaliacao3: Avaliacao = {
+  meta: "Testes.",
+  nota: "MA",
+  copyFrom: null
+}
+const avaliacao4: Avaliacao = {
+  meta: "Refatoração",
+  nota: "MA",
+  copyFrom: null
+}
+​
+alunosStub.forEach((aluno) => {
+  const alunoTemp = new Aluno();
+  alunoTemp.nome = aluno.nome;
+  alunoTemp.email = aluno.email;
+  alunoTemp.cpf = aluno.cpf
+  const matricula = new Matricula();
+  matricula.aluno = alunoTemp;
+  matricula.avaliacoes.push(avaliacao1, avaliacao2, avaliacao3,avaliacao4);
+  matriculasRepo.cadastrarMatricula(matricula);
+})
 
 matriculasRoute.get('/', (req: Request, res: Response) => {
   res.send(matriculasRepo.getMatriculas());
