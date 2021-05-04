dfb024dc6c148b5e05994d7c8f83935637dbd4d6
Belchior <bmf@cin.ufpe.br>
2021-05-04 02:43:15 -0300

    Removendo variavel, refatoração inline temp

--- ta-server/routes/verificaPrazoRoute.ts
+++ ta-server/routes/verificaPrazoRoute.ts
@@ -8,9 +8,9 @@ const verificaPrazoRoute = Router();
 const verificaPrazoRepo: VerificaPrazo = new VerificaPrazo();
 
 verificaPrazoRoute.post('/comparaDataEnvio', function (req: express.Request, res: express.Response) {
-    var AgendamentoRoteiroObject : AgendamentoRoteiro = <AgendamentoRoteiro> req.body; 
-    var resultadoCompare = verificaPrazoRepo.compareDates(AgendamentoRoteiroObject);
-    if (resultadoCompare) {
+    var dateLimitObject : AgendamentoRoteiro = <AgendamentoRoteiro > req.body; 
+    
+    if (verificaPrazoRepo.compareDates(dateLimitObject)) {
       res.send({"success": "seu roteiro foi enviado com sucesso"});
     } else {
       res.send({"failure": "Infelizmente você perdeu o prazo para submissão do roteiro"});
